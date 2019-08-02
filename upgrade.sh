6c6ac6ddbee741d2848159ff53eaae22031a700c
Nerey Sessa <nerey-pro@sessa.com.br>
2019-08-02 16:13:05 -0300

    Remocao completa dir grdk-core

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+cd ..
+rm -Rf ./grdk-core
+mkdir -p grdk-core
+cd grdk-core
 curl -LOk https://github.com/green-pro/grdk-core/archive/master.zip
 unzip master.zip
 rm ./master.zip
