65b2cb7ad548b2970951dfdb7b7c5b4b0bd13b92
Nerey Sessa <nerey-pro@sessa.com.br>
2019-11-28 18:28:45 -0200

    Modificado storage do grdk-proxy certbot para local

--- services/proxy/install_pre.sh
+++ services/proxy/install_pre.sh
@@ -4,15 +4,13 @@ set -e
 ### MANAGER
 if [ "${DK_SERVER_NODE_ROLE}" = "manager" ]; then
 
-	### STORAGE NFS
-	if [ "${DK_SERVER_INST_NFS}" = "Y" ]; then
-		mkdir -p /mnt/storage-1/grdk-proxy
-		mkdir -p /mnt/storage-1/grdk-proxy/certboot
-		mkdir -p /mnt/storage-1/grdk-proxy/certboot/etc
-		mkdir -p /mnt/storage-1/grdk-proxy/certboot/lib
-		mkdir -p /mnt/storage-1/grdk-proxy/certboot/www
-		mkdir -p /mnt/storage-1/grdk-proxy/certboot/www/.well-known/acme-challenge
-	fi
+	### STORAGE LOCAL
+	mkdir -p /mnt/storage-local/grdk-proxy
+	mkdir -p /mnt/storage-local/grdk-proxy/certboot
+	mkdir -p /mnt/storage-local/grdk-proxy/certboot/etc
+	mkdir -p /mnt/storage-local/grdk-proxy/certboot/lib
+	mkdir -p /mnt/storage-local/grdk-proxy/certboot/www
+	mkdir -p /mnt/storage-local/grdk-proxy/certboot/www/.well-known/acme-challenge
 
 fi
 
