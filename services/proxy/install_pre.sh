3912dfd0bea23dc99f6415fa71a59bfdcdf82f9f
Nerey Sessa <nerey-pro@sessa.com.br>
2019-12-12 17:36:17 -0200

    Normalizacao chown apenas para novos dir

--- services/proxy/install_pre.sh
+++ services/proxy/install_pre.sh
@@ -5,12 +5,22 @@ set -e
 if [ "${DK_SERVER_NODE_ROLE}" = "manager" ]; then
 
 	### STORAGE LOCAL
-	mkdir -p /mnt/storage-local/grdk-proxy
-	mkdir -p /mnt/storage-local/grdk-proxy/certboot
-	mkdir -p /mnt/storage-local/grdk-proxy/certboot/etc
-	mkdir -p /mnt/storage-local/grdk-proxy/certboot/lib
-	mkdir -p /mnt/storage-local/grdk-proxy/certboot/www
-	mkdir -p /mnt/storage-local/grdk-proxy/certboot/www/.well-known/acme-challenge
+	if [ -d "/mnt/storage-local/grdk-proxy" ]; then
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/etc
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/lib
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/www
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/www/.well-known/acme-challenge
+	else
+		echo "New dir /mnt/storage-local/grdk-proxy"
+		mkdir -p /mnt/storage-local/grdk-proxy
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/etc
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/lib
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/www
+		mkdir -p /mnt/storage-local/grdk-proxy/certboot/www/.well-known/acme-challenge
+		chown -R root:root /mnt/storage-local/grdk-proxy
+	fi
 
 fi
 
--- services/repo/install_pre.sh
+++ services/repo/install_pre.sh
@@ -6,13 +6,22 @@ if [ "${DK_SERVER_NODE_ROLE}" = "manager" ]; then
 
 	### STORAGE NFS
 	if [ "${DK_SERVER_INST_NFS}" = "Y" ]; then
-		mkdir -p /mnt/storage-1/grdk-repo
-		mkdir -p /mnt/storage-1/grdk-repo/glconf
-		mkdir -p /mnt/storage-1/grdk-repo/gllog
-		mkdir -p /mnt/storage-1/grdk-repo/gldata
-		mkdir -p /mnt/storage-1/grdk-repo/glr1conf
-		mkdir -p /mnt/storage-1/grdk-repo/didata
-		chown -R nobody:nogroup /mnt/storage-1/grdk-repo
+		if [ -d "/mnt/storage-1/grdk-repo" ]; then
+			mkdir -p /mnt/storage-1/grdk-repo/glconf
+			mkdir -p /mnt/storage-1/grdk-repo/gllog
+			mkdir -p /mnt/storage-1/grdk-repo/gldata
+			mkdir -p /mnt/storage-1/grdk-repo/glr1conf
+			mkdir -p /mnt/storage-1/grdk-repo/didata
+		else
+			echo "New dir /mnt/storage-1/grdk-repo"
+			mkdir -p /mnt/storage-1/grdk-repo
+			mkdir -p /mnt/storage-1/grdk-repo/glconf
+			mkdir -p /mnt/storage-1/grdk-repo/gllog
+			mkdir -p /mnt/storage-1/grdk-repo/gldata
+			mkdir -p /mnt/storage-1/grdk-repo/glr1conf
+			mkdir -p /mnt/storage-1/grdk-repo/didata
+			chown -R nobody:nogroup /mnt/storage-1/grdk-repo
+		fi
 	fi
 
 fi
